74d7def3a4f05045e052d9056fcc956b73aa6e52
sngsw111 <lww891110@naver.com>
2020-03-31 16:59:06 +0900

    Dao에서 실수ㅇㅁㅇ!

--- src/main/java/com/example/demo/dao/DeptDao.java
+++ src/main/java/com/example/demo/dao/DeptDao.java
@@ -2,9 +2,11 @@ package com.example.demo.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.db.Manager;
 import com.example.demo.vo.DeptVo;
-
+@Repository
 public class DeptDao {
 	public List<DeptVo> listDept(){
 		return Manager.listDept();
