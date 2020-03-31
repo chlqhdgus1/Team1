f49f5735698cc79a700ed51c1a521e5d5aad5176
sngsw111 <lww891110@naver.com>
2020-03-31 16:17:51 +0900

    Vo Mapper Config 작성

---
+++ src/main/java/com/example/demo/vo/EmpVo.java
@@ -0,0 +1,25 @@
+package com.example.demo.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EmpVo {
+	
+	private int eno;
+	private String ename;
+	private String job;
+	private int sal;
+	private int comm;
+	private int mgr;
+	private Date hiredate;
+	private int dno;
+	private String jumin;
+	private String email;
+}
