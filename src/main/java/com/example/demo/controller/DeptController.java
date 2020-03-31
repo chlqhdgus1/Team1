5c01c15f0b520d9ef4942820499b1135c6fd17e9
hminah0215 <hyeonminah@gmail.com>
2020-03-31 16:15:21 +0900

    static

--- src/main/java/com/example/demo/controller/DeptController.java
+++ src/main/java/com/example/demo/controller/DeptController.java
@@ -1,10 +1,23 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.google.gson.Gson;
+
+@RestController
 public class DeptController {
 	
-	public 
+	@Autowired
+	private DeptDao dao;
 	
+	
+	@RequestMapping("/")
+	public String listDept() {
+		String str ="";
+		Gson gson = new Gson();
+		str = gson.toJson(dao.listAll());
+		return str;
+	}
 }
