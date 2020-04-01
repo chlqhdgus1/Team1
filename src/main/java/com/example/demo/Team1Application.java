16d7ad9ae82c2fa44a1bf2ef3ca29a20c72c7ebe
chlqhdgus1 <chlqhdgus1@naver.com>
2020-04-01 17:09:40 +0900

    메일 스케쥴링 최종본  최봉현

--- src/main/java/com/example/demo/Team1Application.java
+++ src/main/java/com/example/demo/Team1Application.java
@@ -3,13 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableScheduling
 public class Team1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Team1Application.class, args);
-	}
+   public static void main(String[] args) {
+      SpringApplication.run(Team1Application.class, args);
+   }
 
 }
--- src/main/java/com/example/demo/controller/EmailController.java
+++ src/main/java/com/example/demo/controller/EmailController.java
@@ -4,35 +4,30 @@ import java.util.List;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.db.Manager;
 import com.example.demo.vo.EmpVo;
 
-import lombok.Data;
 
 @RestController
 public class EmailController {
-	
-	@Autowired
-	private JavaMailSender mailSender;
+   
+   @Autowired
+private JavaMailSender mailSender;
 	
 	public void setMailSender(JavaMailSender mailSender) {
 		this.mailSender = mailSender;
 	}
 
-	@Scheduled(cron = "0 0 12 1 * *")
+	@Scheduled(cron = "00 01 17 * * *")
 	public void pro() {
 		
 		List<EmpVo> list = Manager.listEmp();
@@ -41,14 +36,13 @@ public class EmailController {
 			sendMail(e);
 		}
 	}
-	
-	@RequestMapping("/mail.do")
+
 	public void sendMail(EmpVo e) {	
 		mailSender.send(new MimeMessagePreparator() {
 			  
 			
 			public void prepare(MimeMessage mimeMessage) throws MessagingException {
-				String content = "<h2>"+"안녕하세요"+e.getEname()+"님의 월급은"+e.getSal() +"입니다.문의 사항은 chlqhdgus1@naver.com 으로 부탁드립니다.</h2>";
+				String content = "<h2>"+"안녕하세요"+e.getEname()+"님의 월급은"+e.getSal() +"입니다.문의 사항은 x..</h2>";
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 			     message.setFrom("chlqhdgus1@gmail.com");
 			     message.setTo(e.getEmail());
@@ -56,7 +50,7 @@ public class EmailController {
 			     content += "<img src ='cid:egg'>";
 			     message.setText(content, true);
 			     message.addInline("egg", new ClassPathResource("static/egg.jpg"));
-			     message.addAttachment("mail.html", new ClassPathResource("static/mail.html"));
+			    //message.addAttachment("NewFile2.html", new ClassPathResource("static/NewFile2.html"));
 			
 			   };
 		});
